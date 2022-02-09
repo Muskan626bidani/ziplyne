018024c52a35888b31f91499795959ebb92699f0
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2022-02-09 16:44:05 +0530

    Updates

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Navbar.css';
-import mylogo from './Ziplynelogo.webp';
-export default function Navbar() {
-  return <div className="navbar">
- <img src={mylogo} className="App-logo" alt="logo" />
-
-  </div>;
-}
